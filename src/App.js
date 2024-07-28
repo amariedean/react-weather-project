d23b88ae25013489fe4064f3b0ea32dee196de3c
amariedean <159481179+amariedean@users.noreply.github.com>
2024-07-27 21:46:05 -0400

    fixing file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/amariedean" target="_blank">
+          Adrianne Dean
+        </a>{" "}
+        and is
+        <a href="" target="_blank">
+          open-sourced on GitHub
         </a>
-      </header>
+        .
+      </footer>
     </div>
   );
 }
